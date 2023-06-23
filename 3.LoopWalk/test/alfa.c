fc44c8b26234c9540b80015350e6a84a1b3f66b2
paoli7612 <paoli7612@gmail.com>
2023-06-23 22:41:43 +0200

    LLVM installation crash

--- 3.LoopWalk/test/alfa.c
+++
@@ -1,9 +0,0 @@
-void foo() {
-
-  int a = 7;
-  int b = 6;
-  
-  int somma = a + 0;
-  somma = somma + b;
-
-}
--- 3.LoopWalk/test/beta.c
+++
@@ -1,9 +0,0 @@
-void foo() {
-
-  int contatore = 0;
-  for (int i=0; i<10; i++) {
-    contatore += 2;
-  }
-  int risultato = contatore;
-
-}
